bce2721bd000f963eff04a26f92e5d55c9f4aebb
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-13 13:26:31 -0700

    created a Space comp

---
+++ src/components/Space.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Space() {
+
+  const styles = {
+    backgroundColor: 'black',
+    height: '15px',
+    width: '80vw',
+    marginTop: '20px',
+    marginBottom: '20px',
+    marginLeft: '10vw'
+  };
+
+  return (
+    <>
+      <div style={styles}></div>
+    </>
+  );
+}
