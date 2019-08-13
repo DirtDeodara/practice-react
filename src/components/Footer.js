a0dc3287f48741ffbcfc66c724771abaab154f6d
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-13 13:26:45 -0700

    created a Footer comp

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Footer() {
+  
+  const styles = {
+    backgroundColor: 'black',
+    height: '50px',
+    width: '100vw',
+    marginTop: '20px',
+  };
+
+  return (
+    <>
+      <footer style={styles}></footer>
+    </>
+  );
+
+}
