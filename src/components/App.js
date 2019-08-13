6b5fc047338e230c7cd4dce458259e6fe4ef81e9
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-13 12:49:32 -0700

    brought in all of my components to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import Dog from './Dog';
+import Header from './Header';
+import Color from './Color';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+      <Dog name="Harvey" age={9} weight="55 lbs" />
+      <Color name="lava" rgb="207, 16, 32" hex="#cf1020"/>
+    </>
+  );
 }
-  
