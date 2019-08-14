4b476bdc6fc0f19e03833a1fe9119a95a91c1ff8
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-14 08:59:27 -0700

    moved subs array out of app and into Subs

--- src/components/App.js
+++ src/components/App.js
@@ -5,42 +5,19 @@ import Color from './Color';
 import Space from './Space';
 import Footer from './Footer';
 import Subs from './Subs';
-import Button from './Button';
 
 export default function App() {
-  const subsArr = [
-    {
-      name: 'kimura',
-      type: 'shoulder lock'
-    },
-    {
-      name: 'RNC',
-      type: 'strangle'
-    },
-    {
-      name: 'knee Bar',
-      type: 'joint lock'
-    },
-    
-  ];
-
-  const colorProps = {
-    name: 'lava',
-    rgb: '207, 16, 32',
-    hex: '#cf1020',
-  };
 
   return (
     <>
       <Header />
       <Space />
-      <Button />
       <Space />
       <Dog name="Harvey" age={9} weight="55 lbs" />
       <Space />
       <Color name="lava" rgb="207, 16, 32" hex="#cf1020"/>
       <Space />
-      <Subs subsArr={subsArr} colorProps={colorProps}/>
+      <Subs />
       <Footer />
     </>
   );
